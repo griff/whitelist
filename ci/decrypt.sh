360da23e0e21046afe3169450f9b3b5ae8852c21
Brian Olsen <brian@maven-group.org>
2019-01-04 02:36:38 +0100

    Try to fix deployment

--- ci/decrypt.sh
+++ ci/decrypt.sh
@@ -2,8 +2,10 @@
 set -o errexit
 set -v
 
-openssl aes-256-cbc -K $encrypted_241552de5a9b_key -iv $encrypted_241552de5a9b_iv -in keypair.ucl.enc -out keypair.ucl -d
-openssl aes-256-cbc -K $encrypted_241552de5a9b_key -iv $encrypted_241552de5a9b_iv -in id_ed25519.enc -out /tmp/id_ed25519 -d
-chmod 600 /tmp/id_ed25519
-eval "$(ssh-agent -s)"
-ssh-add /tmp/id_ed25519
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] ; then
+  openssl aes-256-cbc -K "$encrypted_241552de5a9b_key" -iv "$encrypted_241552de5a9b_iv" -in keypair.ucl.enc -out keypair.ucl -d
+  openssl aes-256-cbc -K "$encrypted_241552de5a9b_key" -iv "$encrypted_241552de5a9b_iv" -in id_ed25519.enc -out /tmp/id_ed25519 -d
+  chmod 600 /tmp/id_ed25519
+  eval "$(ssh-agent -s)"
+  ssh-add /tmp/id_ed25519
+fi
