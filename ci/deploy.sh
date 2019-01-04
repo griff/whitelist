c2c4142778d1bb3c80564a2c36a847caf5f855c8
Brian Olsen <brian@maven-group.org>
2019-01-04 03:02:37 +0100

    Fiddle with SSH settings to try and get deploy to work

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,4 +3,10 @@ set -o errexit
 set -v
 
 ./ci/sign.sh
+
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add /tmp/id_ed25519
+fi
+ls -la $HOME/.ssh/
 rsync -rli --progress --checksum output/ brian@xev.maven-group.org:/data/xev-files.maven-group.org/whitelist/
