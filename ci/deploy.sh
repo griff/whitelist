1fa3ed4ca2e1b8433913944fafea1896f9b95824
Brian Olsen <brian@maven-group.org>
2019-01-04 03:06:25 +0100

    Fix .ssh/cofig permissions

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,5 +8,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] ; then
   eval "$(ssh-agent -s)"
   ssh-add /tmp/id_ed25519
 fi
+chmod 600 $HOME/.ssh/config
 ls -la $HOME/.ssh/
+cat $HOME/.ssh/config
 rsync -rli --progress --checksum output/ brian@xev.maven-group.org:/data/xev-files.maven-group.org/whitelist/
