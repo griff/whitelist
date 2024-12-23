4d9211d2f305aea2b199e08fb929662038416dc2
griff <griff@users.noreply.github.com>
2024-12-23 12:08:55 +0000

    [update-ruby-gems] automated change

--- gemset.nix
+++ gemset.nix
@@ -802,10 +802,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1kvbzh8530pp3qf63zvx9hnb708x7plv9wfn5ibns3h3knnvs3kw";
+      sha256 = "048danb0x10mpch6mf88mky35zjn6wk4hpbqq68ssbq58i3fzgfj";
       type = "gem";
     };
-    version = "2.9.0";
+    version = "2.9.1";
   };
   kramdown = {
     dependencies = ["rexml"];
@@ -855,10 +855,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1574gi74z5pww36rv0jvqlv9ybm87h7c37fb5r2axn3mbh0wwcs5";
+      sha256 = "1rrf3y8j3fjjmn74d2i3l85pjm7yhvl8xgz7684hac92j8fbj9xn";
       type = "gem";
     };
-    version = "1.6.3";
+    version = "1.6.4";
   };
   mercenary = {
     source = {
